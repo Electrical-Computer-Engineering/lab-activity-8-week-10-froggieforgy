4f9d339bd6d2224f2551961e9c04c68ab17b5974
froggieforgy <jianyu.zhang@stonybrook.edu>
2024-04-05 00:15:43 -0400

    My first submission

--- part1_question1.c
+++ part1_question1.c
@@ -1,1 +1,53 @@
+//Damon Zhang SBUID:115931039
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int my_isalpha(char c) {
+	if (c >= 'a' && c < 'z' || c >= 'A' && c < 'Z')
+	return 1;
+	else 
+	return 0;
+}
+//notes for self: return c >= 'a' && c < 'z' || c >= 'A' && c < 'Z' does the
+//same thing b/c if that condition is true in that f statement then it's 1
+//so return c >= 'a' && c < 'z' || c >= 'A' && c < 'Z' is the longer version
+//of what you just did
+int my_isdigit(char c){
+	if (c >= '0' || c<= '9')
+	return 1;
+	else
+	return 0;
+}
+int my_is_upper(char c){
+	if (c >= 'A' && c < 'Z')
+	return 1;
+	else 
+	return 0;
+}
+char my_to_upper(char c){
+	if (c >= 'a' && c <= 'z')
+	c = c - 'a' + 'A';
+	return c;
+}
+char my_to_lower (char c){
+	if (c >= 'A' && c <= 'Z')
+	c = c - 'A' + 'a';
+	return c; 
+}
+
+int main(){
+	char c;
+	int i;
+
+	for (i=0; i<2; i++){
+	printf("Give me c ");scanf("%c",&c); getchar();
+	printf("%d\n", my_isalpha(c));
+	printf("%d\n", my_isdigit(c));
+	printf("%d\n",my_is_upper(c));
+	printf("%c\n",my_to_upper(c));
+	printf("%c\n",my_to_lower(c));
+	}	
+}
+
+//COMPLETE; NO ERRORS SPOTTED
--- part2_question1.c
+++ part2_question1.c
@@ -1,1 +1,47 @@
+//Damon Zhang SBUID:115931039
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int my_strcmp(char str1[], char str2[]){
+	for(i=0;str1[i] != '\0' && str2[i]!= '\0'; i++){
+		if str1[i] == str2[i]
+		return 0;
+		else
+		return 1;
+	}
+}
+int my_strncmp(char str1[], char str2[]){
+	printf("what is n"); scanf("%d", &n);
+		for(i=0;i<n; i++){
+		if str1[i] == str2[i]
+		return 0;
+		else
+		return 1;
+	}
+}
+char *my_strcpy(char str1[], char str2[]){
+	char placeholder[]=str1[];
+	str1[]=str2[];
+	printf("%s",placeholder[]);
+	return placeholder[];
+}
+
+char my_strcat(char str1[], char str2[]){
+	return str1[];
+	for(i=0;str1[i] != '\0'; i++)
+		
+}
+char my_reverse(char str1[], char str2[]){
+	
+}
+
+
+int main(){
+	int i;
+	char str1 []
+	
+	
+	
+	
+}
